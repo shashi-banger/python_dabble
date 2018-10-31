1d1a024a20bc9e928403bfbc51f3c86921c9c6e0
shashi-banger <shashidhar@amagi.com>
2018-10-31 09:47:25 +0530

    Simplified api

--- grpc/client.py
+++ grpc/client.py
@@ -12,9 +12,10 @@ def run():
         stub = example_pb2_grpc.RecorderStub(channel)
         track_v = example_pb2.TrackDesc(id=0, pid=2064, type="video", codec="mpeg2video", tag="img_main_vid", resolution="720x576", frame_rate=25)
         track_a = example_pb2.TrackDesc(id=1, pid=2068, type="audio", codec="aac", tag="img_main_aud", sampling_rate=48000)
-        response = stub.StartRecorder(example_pb2.RecorderParams(event_id='abcdefg',
-                                ingest_name="img_main", udp_url="udp://127.0.0.1:1234", primary=True, \
-                                tracks=[track_v, track_a]))
+        recorder_params = example_pb2.RecorderParams(event_id='abcdefg',
+                     ingest_name="img_main", udp_url="udp://127.0.0.1:1234", primary=True, \
+                     tracks=[track_v, track_a])
+        response = stub.StartRecorder(recorder_params)
     print("Recorder client received: " + response.message)
 
 
